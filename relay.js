a30d22ab5b7ea46c5f60e4fa3a20dd8af7bfae52
simon3000 <simon30002021@icloud.com>
2020-08-04 21:30:20 +0200

    show me the log

--- relay.js
+++ relay.js
@@ -130,6 +130,7 @@ bot.on('message.group', async (_, ctx, tags) => {
         .filter(tag => tag instanceof CQImage)
         .map(({ file }) => bot('get_image', { file }))
         .map(async p => {
+          console.log(await p)
           const { filename, url } = await p
           return new Discord.Attachment(got.stream(url), filename)
         }))
